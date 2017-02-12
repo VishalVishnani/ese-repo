4500639a31fb85bd75f05edb13ee05d1954ab49f
Virag Gada <virag.gada@colorado.edu>
2017-02-11 22:57:53 -0700

    Adding project_1.h to project1

---
+++ project1/project_1.h
@@ -0,0 +1,8 @@
+#ifndef PROJECT_1_H_INCLUDED
+#define PROJECT_1_H_INCLUDED
+
+void project_1_report(void);
+void test_data1(void);
+void test_data2(void);
+void test_memory(void);
+#endif // PROJECT_1_H_INCLUDED
