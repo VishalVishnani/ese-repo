e23d87d450cc01883e56dfd5576f335b5974a97f
Vishal Vishnani <vivi7647@colorado.edu>
2017-02-16 16:10:24 -0700

    Edited sources.mk

--- project1/sources.mk
+++ project1/sources.mk
@@ -1,5 +1,5 @@
 VPATH=SOURCES
 
-SRCS=main.c project_1.c data.c memory.c
-HDR=data.h project_1.h memory.h
-OBJS=memory.o data.o project_1.o main.o
+SRCS=main.c project_1.c data.c memory.c test.c
+HDR=data.h project_1.h memory.h test.h
+OBJS=memory.o data.o project_1.o test.o main.o
