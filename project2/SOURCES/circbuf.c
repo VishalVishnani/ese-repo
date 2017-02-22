ace3d3535693cf1a217ab9f39708d06b9064496c
Virag Gada <virag.gada@colorado.edu>
2017-02-21 22:54:28 -0700

    Adding circbuf.c

---
+++ project2/SOURCES/circbuf.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+
+CB_e CB_AddItem(CB * buf, ...)
+{
+
+}
+
+CB_e CB_RemoveItem(CB * buf, ...)
+{
+
+}
+
+CB_e CB_Full(CB * buf, ...)
+{
+
+}
+
+CB_e CB_Empty(CB * buf, ...)
+{
+
+}
+
+CB_e CB_Peak(CB * buf, ...)
+{
+
+}
+
+CB_e CB_Initialize(CB * buf, ...)
+{
+
+}
+
+CB_e CB_Destroy(CB * buf, ...)
+{
+
+}
