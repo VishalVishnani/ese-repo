9dd110e479b16ff360d2566fdb6525111d9e3dab
Vishal Vishnani <vivi7647@colorado.edu>
2017-03-02 22:41:14 -0700

    Adding cmocka libs and deleting files

---
+++ project2/SOURCES/Libraries/build-Debug/cmocka/example/CMakeFiles/calculator_test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vishal/Documents/Unit Test/Libraries/cmocka/example/calculator.c" "/home/vishal/Documents/Unit Test/Libraries/build-Debug/cmocka/example/CMakeFiles/calculator_test.dir/calculator.c.o"
+  "/home/vishal/Documents/Unit Test/Libraries/cmocka/example/calculator_test.c" "/home/vishal/Documents/Unit Test/Libraries/build-Debug/cmocka/example/CMakeFiles/calculator_test.dir/calculator_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "example"
+  "/home/vishal/Documents/Unit Test/Libraries/cmocka/example"
+  "."
+  "/home/vishal/Documents/Unit Test/Libraries/cmocka/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vishal/Documents/Unit Test/Libraries/build-Debug/cmocka/src/CMakeFiles/cmocka_shared.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
