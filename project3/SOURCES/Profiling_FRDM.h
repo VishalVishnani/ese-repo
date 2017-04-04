1d1d1939ef1716d4b27b8b2f609091e5faf96c9d
Vishal Vishnani <vivi7647@colorado.edu>
2017-04-03 21:21:47 -0600

    Adding timer profiler header for FRDM

---
+++ project3/SOURCES/Profiling_FRDM.h
@@ -0,0 +1,19 @@
+/*
+ * Profiling_FRDM.h
+ *
+ *  Created on: Apr 2, 2017
+ *      Author: vishal
+ */
+//MCGFLL clock frequency 20.97 MHZ
+
+#ifndef SOURCES_PROFILING_FRDM_H_
+#define SOURCES_PROFILING_FRDM_H_
+
+
+void init_TPM0();
+void TPM0_start();
+void TPM0_stop();
+
+
+
+#endif /* SOURCES_PROFILING_FRDM_H_ */
