67ca3c3095609628645c6c890054607447a12abb
Virag Gada <virag.gada@colorado.edu>
2017-04-07 15:28:52 -0600

    Adding rtc.h to SOURCES

---
+++ project3/SOURCES/rtc.h
@@ -0,0 +1,15 @@
+/*
+ * rtc.h
+ *
+ *  Created on: 27-Mar-2017
+ *      Author: omkar
+ */
+
+#ifndef SOURCES_RTC_H_
+#define SOURCES_RTC_H_
+
+void rtc_init(void);
+
+
+
+#endif /* SOURCES_RTC_H_ */
