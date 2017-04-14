8b52afe584fb55f216736a2001ab6f923629cc2b
Virag Gada <virag.gada@colorado.edu>
2017-04-13 23:11:40 -0600

    Updating rtc.h

--- project3/SOURCES/rtc.h
+++ project3/SOURCES/rtc.h
@@ -8,8 +8,17 @@
 #ifndef SOURCES_RTC_H_
 #define SOURCES_RTC_H_
 
+#include <time.h>
+
+#define EPOCH_TIME (1492146476)
+
+
+time_t current_time;
+uint8_t * c_time_string;
+
+/*******************************************
+* SPI_init() - Function to initialize RTC
+********************************************/
 void rtc_init(void);
-
-
 
 #endif /* SOURCES_RTC_H_ */
