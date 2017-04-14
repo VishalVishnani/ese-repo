3440540a67d7b29bbce24a561ca1901e444031d0
Virag Gada <virag.gada@colorado.edu>
2017-04-13 23:05:38 -0600

    Adding gpio.h to SOURCES

---
+++ project3/SOURCES/gpio.h
@@ -0,0 +1,16 @@
+/*
+ * gpio.h
+ *
+ *  Created on: Apr 12, 2017
+ *      Author: Virag Gada
+ */
+
+#ifndef SOURCES_GPIO_H_
+#define SOURCES_GPIO_H_
+
+#define ALT_MODE (0x0000200)
+
+/*Function to initialize gpio pins for NRF module*/
+void GPIO_nrf_init();
+
+#endif /* SOURCES_GPIO_H_ */
