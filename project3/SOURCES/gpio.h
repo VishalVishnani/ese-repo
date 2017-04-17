85a517b2b65e03799e73ec3fd988fdbaa71e8600
Virag Gada <virag.gada@colorado.edu>
2017-04-16 22:40:25 -0600

    Updating gpio.h

--- project3/SOURCES/gpio.h
+++ project3/SOURCES/gpio.h
@@ -1,9 +1,12 @@
-/*
- * gpio.h
- *
- *  Created on: Apr 12, 2017
- *      Author: Virag Gada
- */
+/*************************************************************************
+* Authors : Vishal Vishnani, Virag Gada
+* Date : 04/12/2017
+*
+* File : gpio.h
+* Description : Header file for GPIO initialize functions
+*               - void GPIO_nrf_init()
+*               - void GPIO_led_init()
+***************************************************************************/
 
 #ifndef SOURCES_GPIO_H_
 #define SOURCES_GPIO_H_
@@ -11,6 +14,20 @@
 #define ALT_MODE (0x0000200)
 
 /*Function to initialize gpio pins for NRF module*/
+/*********************************************************************
+* GPIO_nrf_init() - Function to initialize GPIO pins for NRF module
+*
+*********************************************************************/
 void GPIO_nrf_init();
+
+
+#define PIN_18	(0x400000)
+
+/*********************************************************************
+* GPIO_led_init() - Function to initialize gpio pins for RED LED module
+*
+*********************************************************************/
+void GPIO_led_init();
+
 
 #endif /* SOURCES_GPIO_H_ */
