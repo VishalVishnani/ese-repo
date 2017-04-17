8898ee9aa85c6dc6d38f83d1f247a2563f0a782b
Vishal Vishnani <vivi7647@colorado.edu>
2017-04-16 23:55:23 -0600

    Adding sources.mk

---
+++ project3/sources.mk
@@ -0,0 +1,7 @@
+VPATH=SOURCES
+
+SRCS=binary_logger.c circbuff.c main.c memory.c dma.c gpio.c logger.c nordic.c Profiling_FRDM.c rtc.c spi.c data.c uart_init.c
+
+HDR=binary_logger.h circbuff.h memory.h dma.h gpio.h logger.h nordic.h Profiling_FRDM.h rtc.h spi.h data.h uart_init.h
+
+OBJS=binary_logger.o circbuff.o main.o memory.o dma.o gpio.o logger.o nordic.o Profiling_FRDM.o rtc.o spi.o data.o uart_init.o
