850eee7b206d37116a984f5afec6d29ab88ca819
Virag Gada <virag.gada@colorado.edu>
2017-04-27 19:38:32 -0600

    Changing to SPI1 on Port D

--- project4/SOURCES/gpio.c
+++ project4/SOURCES/gpio.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 * Authors : Vishal Vishnani, Virag Gada
-* Date : 04/12/2017
+* Date : 04/27/2017
 *
 * File : gpio.c
 * Description : Source file for GPIO initialize functions
@@ -23,31 +23,31 @@ void GPIO_nrf_init(){
 	//Enabling clock to Port D
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 
-	//Provide clock to SPI0
-	SIM_SCGC4 |= SIM_SCGC4_SPI0_MASK;
+	//Provide clock to SPI1
+	SIM_SCGC4 |= SIM_SCGC4_SPI1_MASK;
 
 	//CE ON by default
-	PORTD_PCR5 = PORT_PCR_MUX(1);				//Set the PTD5 pin multiplexer to GPIO mode
+	PORTD_PCR2 = PORT_PCR_MUX(1);				//Set the PTD2 pin multiplexer to GPIO mode
 	GPIOD_PDDR |= CHIP_SHIFT;					// Set the pins direction to output
 	GPIOD_PCOR |= CHIP_SHIFT;					//Set the initial output state to low
 
 	//CSN is HIGH by default
-	PORTD_PCR6 = PORT_PCR_MUX(1);				//Set the PTD6 pin multiplexer to GPIO mode
+	PORTD_PCR3 = PORT_PCR_MUX(1);				//Set the PTD3 pin multiplexer to GPIO mode
 	GPIOD_PDDR |= SLAVE_SHIFT;					//Set the pins direction to output
 	GPIOD_PSOR |= SLAVE_SHIFT;					//Set the initial output state to high
 
 	//For IRQ from the SPI device
-	PORTD_PCR7 = PORT_PCR_MUX(1);				//Set the PTD7 pin multiplexer to GPIO mode
+	PORTD_PCR0 = PORT_PCR_MUX(1);				//Set the PTD0 pin multiplexer to GPIO mode
 	GPIOD_PDDR |= ISR_SHIFT;					//Set the pins direction to input
 
-	//Set as SPI0_SCK
-	PORTD->PCR[1]= ALT_MODE;
+	//Set as SPI1_SCK
+	PORTD->PCR[5]= ALT_MODE;
 
-	//Set as SPI0_MOSI
-	PORTD->PCR[2]= ALT_MODE;
+	//Set as SPI1_MOSI
+	PORTD->PCR[6]= ALT_MODE;
 
-	//Set as SPI0_MISO
-	PORTD->PCR[3]= ALT_MODE;
+	//Set as SPI1_MISO
+	PORTD->PCR[7]= ALT_MODE;
 
 	#ifdef LOG_ON
 		/*Binary logger for GPIO_INITIALIZED*/
