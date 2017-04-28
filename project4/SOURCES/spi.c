63d27a0fafb57dc9de0b3885c3bb8585bed3942b
Virag Gada <virag.gada@colorado.edu>
2017-04-27 19:36:06 -0600

    Changing to SPI1 on Port D

--- project4/SOURCES/spi.c
+++ project4/SOURCES/spi.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 * Authors : Vishal Vishnani, Virag Gada
-* Date : 04/10/2017
+* Date : 04/27/2017
 *
 * File : spi.c
 * Description : Source file for SPI functions
@@ -20,28 +20,28 @@
 #include "spi.h"
 #include "gpio.h"
 
-/* Function to initialize SPI0 */
+/* Function to initialize SPI1 */
 void SPI_init(){
 
 	GPIO_nrf_init();
 
 	//Set as Master
-	SPI0_C1 = SPI_C1_MSTR_MASK;
+	SPI1_C1 = SPI_C1_MSTR_MASK;
 
 	//Make Master SS pin to select slave output
-	SPI0_C2 = SPI_C2_MODFEN_MASK;
+	SPI1_C2 = SPI_C2_MODFEN_MASK;
 
 	//Set baud rate
-	SPI0_BR = (SPI_BR_SPPR(0x02) | SPI_BR_SPR(0x01)); //Set Bitrate to 1Mbps
+	SPI1_BR = (SPI_BR_SPPR(0x02) | SPI_BR_SPR(0x01)); //Set Bitrate to 1Mbps
 
-	//Enable SPI0
-	SPI0_C1 |= SPI_C1_SPE_MASK;
+	//Enable SPI1
+	SPI1_C1 |= SPI_C1_SPE_MASK;
 }
 
 /*Function to read a byte using SPI*/
 void SPI_read_byte(uint8_t * byte){
-	while(!(SPI0->S & SPI_S_SPRF_MASK));
-	*byte = SPI0->D;
+	while(!(SPI1->S & SPI_S_SPRF_MASK));
+	*byte = SPI1->D;
 }
 
 /*Function to write a byte using SPI*/
@@ -49,7 +49,7 @@ uint8_t SPI_write_byte(uint8_t byte){
 	uint8_t val;
 	//while(!(SPI0->S & SPI_S_SPTEF_MASK));
 	SPI_flush();
-	SPI0->D = byte;
+	SPI1->D = byte;
 	SPI_read_byte(&val);
 	return val;
 }
@@ -66,7 +66,7 @@ void SPI_send_packet(uint8_t * p, size_t length){
 
 /*Function to block till SPI transmit buffer is empty*/
 void SPI_flush(){
-	while(!(SPI0->S & SPI_S_SPTEF_MASK));
+	while(!(SPI1->S & SPI_S_SPTEF_MASK));
 }
 
 #endif
